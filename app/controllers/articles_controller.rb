e18b69fc2cc4e2b37316a73405d746f623343aab
Stefanus Likardi <stefanus.likardi@nutrifood.co.id>
2022-03-16 13:10:13 +0700

    Add association for articles and user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,7 @@ class ArticlesController < ApplicationController
 
     def create 
         @article = Article.new(article_params)
+        @article.user = User.first
         # render plain: @article.inspect
         if @article.save
             flash[:notice] = "Article was created successfully."
